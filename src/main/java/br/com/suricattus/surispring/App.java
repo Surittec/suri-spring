f9354c700ec8ecb70ff1b7bd9411f17c52fa131e
Lucas Lins <lucas.lins@suricattus.com.br>
2012-05-02 18:14:03 -0300

    criando o projeto

---
+++ src/main/java/br/com/suricattus/surispring/App.java
@@ -0,0 +1,13 @@
+package br.com.suricattus.surispring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
