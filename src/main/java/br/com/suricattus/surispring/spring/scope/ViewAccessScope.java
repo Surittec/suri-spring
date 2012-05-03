a7d5866b1b54cfc144b57acd7e488fcad7248730
Lucas Lins <lucas.lins@suricattus.com.br>
2012-05-02 21:28:25 -0300

    including LGPL license

--- src/main/java/br/com/suricattus/surispring/spring/scope/ViewAccessScope.java
+++ src/main/java/br/com/suricattus/surispring/spring/scope/ViewAccessScope.java
@@ -1,3 +1,23 @@
+/*
+ * SURICATTUS
+ * Copyright 2011, SURICATTUS CONSULTORIA LTDA, 
+ * and individual contributors as indicated by the @authors tag
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package br.com.suricattus.surispring.spring.scope;
 
 import java.util.Map;
@@ -10,6 +30,11 @@ import org.springframework.web.jsf.FacesContextUtils;
 
 import br.com.suricattus.surispring.spring.scope.holder.ViewAccessScopeHolder;
 
+/**
+ * 
+ * @author Lucas Lins
+ *
+ */
 public class ViewAccessScope extends ViewScope{
 
 	public static final String CALLBACKS = ViewAccessScope.class.getName() + ".callbacks";
--- src/main/java/br/com/suricattus/surispring/spring/scope/ViewScope.java
+++ src/main/java/br/com/suricattus/surispring/spring/scope/ViewScope.java
@@ -1,3 +1,23 @@
+/*
+ * SURICATTUS
+ * Copyright 2011, SURICATTUS CONSULTORIA LTDA, 
+ * and individual contributors as indicated by the @authors tag
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package br.com.suricattus.surispring.spring.scope;
 
 import java.util.Map;
@@ -8,6 +28,11 @@ import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.config.Scope;
 import org.springframework.web.context.request.FacesRequestAttributes;
 
+/**
+ * 
+ * @author Lucas Lins
+ *
+ */
 public class ViewScope implements Scope{
 
 	public static final String CALLBACKS = ViewScope.class.getName() + ".callbacks";
