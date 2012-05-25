46d21170c848cb207feb2a1f15c41991ea2d52c1
Lucas Lins <lucas.lins@suricattus.com.br>
2012-05-25 00:16:16 -0300

    developing

--- src/main/java/br/com/suricattus/surispring/framework/util/CnpjUtil.java
+++ src/main/java/br/com/suricattus/surispring/framework/util/CnpjUtil.java
@@ -9,11 +9,11 @@ public abstract class CnpjUtil {
 	// ATTIRBUTES
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	
-	private static List<String> cpfInvalidos = Arrays.asList(new String []{"00000000000", "11111111111",
-																		   "22222222222", "33333333333",
-																		   "44444444444", "55555555555",
-																		   "66666666666", "77777777777",
-																		   "88888888888", "99999999999"});
+	private static List<String> cnpjInvalidos = Arrays.asList(new String []{"00000000000000","11111111111111",
+																			"22222222222222","33333333333333",
+																			"44444444444444","55555555555555",
+																			"66666666666666","77777777777777",
+																			"88888888888888","99999999999999"});
 	
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	// PUBLIC METHODS
@@ -24,16 +24,19 @@ public abstract class CnpjUtil {
 	}
 	
 	public static boolean isValid(Object value, boolean nullable) {
-		
 		if(!nullable && value == null) return false;
-
+		
 		if(nullable && value == null) return true;
 		
-		if (value != null && !isValidCnpj(value.toString())) {
-			return false;
-		}else{
-			return true;
+		if (value instanceof String) {
+			String cnpj = (String) value;
+			String regex = "\\d{14}";
+			if (!cnpj.matches(regex)){
+				return false;
+			}
+			return isValidCnpj(cnpj);
 		}
+		return false;
 	}
 	
 	public static String format(String cnpj){
@@ -57,31 +60,59 @@ public abstract class CnpjUtil {
 	
 	private static boolean isValidCnpj(String cnpj) {
 		
-		if(cpfInvalidos.contains(cnpj)) return false;
+		if(cnpjInvalidos.contains(cnpj)) return false;
 		
-		if (cnpj.trim().length() != 11) return false;
+		char charsCNPJ[] = cnpj.toCharArray();
+		Integer digito1 = calculoPrimeiroDigitoVerificador(charsCNPJ);
+		Integer digito2 = calculoSegundoDigitoVerificador(charsCNPJ, digito1);
 
-		String cpf1 = cnpj.substring(0, 9); // rcpf1
-		String cpf2 = cnpj.substring(9); // rcpf2
-		int i;
+		String digitoIdentificadorCalculado = digito1.toString() + digito2.toString();
+		String digitoIdentificadorInformado = charsCNPJ[charsCNPJ.length - 2] + "" + charsCNPJ[charsCNPJ.length - 1];
 
-		int d1 = 0;
-		for (i = 0; i < 9; i++) d1 += Integer.parseInt(cpf1.substring(i, i + 1)) * (10 - i);
+		return digitoIdentificadorCalculado.equals(digitoIdentificadorInformado);
+	}
 
-		d1 = 11 - (d1 % 11);
-		if (d1 > 9) d1 = 0;
+	private static int calculoPrimeiroDigitoVerificador(char[] charsCNPJ) {
+		int fator = 5;
+		int soma = 0;
+		for (int i = 0; i < charsCNPJ.length - 2; i++) {
+			char c = charsCNPJ[i];
+			soma += Integer.parseInt(String.valueOf(c)) * fator;
+			if (i == 3){
+				fator = 9;
+			}else{
+				fator--;
+			}
+		}
 
-		if (Integer.parseInt(cpf2.substring(0, 1)) != d1) return false;
+		int valor = (soma) % 11;
 
-		d1 *= 2;
-		for (i = 0; i < 9; i++) d1 += Integer.parseInt(cpf1.substring(i, i + 1)) * (11 - i);
+		int digito1 = 11 - valor;
+		if ((soma) % 11 < 2) {
+			digito1 = 0;
+		}
+		return digito1;
+	}
 
-		d1 = 11 - (d1 % 11);
-		if (d1 > 9) d1 = 0;
+	private static int calculoSegundoDigitoVerificador(char[] charsCNPJ, int digito1) {
+		int fator2 = 6;
+		int soma2 = 0;
+		for (int i = 0; i < charsCNPJ.length - 2; i++) {
+			char c = charsCNPJ[i];
+			soma2 += Integer.parseInt(String.valueOf(c)) * fator2;
+			if (i == 4){
+				fator2 = 9;
+			}else{
+				fator2--;
+			}
+		}
+		soma2 += digito1 * 2;
+		int valor2 = (soma2) % 11;
 
-		if (Integer.parseInt(cpf2.substring(1, 2)) != d1) return false;
-
-		return true;
+		int digito2 = 11 - valor2;
+		if ((soma2) % 11 < 2)
+			digito2 = 0;
+		return digito2;
 	}
 	
 }
--- src/main/java/br/com/suricattus/surispring/framework/util/CpfUtil.java
+++ src/main/java/br/com/suricattus/surispring/framework/util/CpfUtil.java
@@ -34,11 +34,11 @@ public abstract class CpfUtil {
 	// ATTIRBUTES
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	
-	private static List<String> cnpjInvalidos = Arrays.asList(new String []{"00000000000000", "11111111111111",
-																			"22222222222222", "33333333333333",
-																			"44444444444444", "55555555555555",
-																			"66666666666666", "77777777777777",
-																			"88888888888888", "99999999999999"});
+	private static List<String> cpfInvalidos = Arrays.asList(new String []{	"00000000000","11111111111",
+																			"22222222222","33333333333",
+																			"44444444444","55555555555",
+																			"66666666666","77777777777",
+																			"88888888888","99999999999"});
 	
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	// PUBLIC METHODS
@@ -50,18 +50,14 @@ public abstract class CpfUtil {
 	
 	public static boolean isValid(Object value, boolean nullable) {
 		if(!nullable && value == null) return false;
-		
+
 		if(nullable && value == null) return true;
 		
-		if (value instanceof String) {
-			String cnpj = (String) value;
-			String regex = "\\d{14}";
-			if (!cnpj.matches(regex)){
-				return false;
-			}
-			return cnpjValido(cnpj);
+		if (value != null && !isValidCpf(value.toString())) {
+			return false;
+		}else{
+			return true;
 		}
-		return false;
 	}
 	
 	public static String format(String cpf){
@@ -85,61 +81,45 @@ public abstract class CpfUtil {
 	// PRIVATE METHODS
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	private static boolean cnpjValido(String cnpj) {
+	private static boolean isValidCpf(String cpf) {
 		
-		if(cnpjInvalidos.contains(cnpj)) return false;
+		if(cpfInvalidos.contains(cpf)) return false;
 		
-		char charsCNPJ[] = cnpj.toCharArray();
-		Integer digito1 = calculoPrimeiroDigitoVerificador(charsCNPJ);
-		Integer digito2 = calculoSegundoDigitoVerificador(charsCNPJ, digito1);
+		if (cpf.trim().length() != 11) return false;
 
-		String digitoIdentificadorCalculado = digito1.toString() + digito2.toString();
-		String digitoIdentificadorInformado = charsCNPJ[charsCNPJ.length - 2] + "" + charsCNPJ[charsCNPJ.length - 1];
+		String cpf1 = cpf.substring(0, 9); // rcpf1
+		String cpf2 = cpf.substring(9); // rcpf2
+		int i;
 
-		return digitoIdentificadorCalculado.equals(digitoIdentificadorInformado);
-	}
-
-	private static int calculoPrimeiroDigitoVerificador(char[] charsCNPJ) {
-		int fator = 5;
-		int soma = 0;
-		for (int i = 0; i < charsCNPJ.length - 2; i++) {
-			char c = charsCNPJ[i];
-			soma += Integer.parseInt(String.valueOf(c)) * fator;
-			if (i == 3){
-				fator = 9;
-			}else{
-				fator--;
-			}
+		int d1 = 0;
+		for (i = 0; i < 9; i++) {
+			d1 += Integer.parseInt(cpf1.substring(i, i + 1)) * (10 - i);
 		}
 
-		int valor = (soma) % 11;
-
-		int digito1 = 11 - valor;
-		if ((soma) % 11 < 2) {
-			digito1 = 0;
+		d1 = 11 - (d1 % 11);
+		if (d1 > 9) {
+			d1 = 0;
 		}
-		return digito1;
-	}
 
-	private static int calculoSegundoDigitoVerificador(char[] charsCNPJ, int digito1) {
-		int fator2 = 6;
-		int soma2 = 0;
-		for (int i = 0; i < charsCNPJ.length - 2; i++) {
-			char c = charsCNPJ[i];
-			soma2 += Integer.parseInt(String.valueOf(c)) * fator2;
-			if (i == 4){
-				fator2 = 9;
-			}else{
-				fator2--;
-			}
+		if (Integer.parseInt(cpf2.substring(0, 1)) != d1) {
+			return false;
 		}
-		soma2 += digito1 * 2;
-		int valor2 = (soma2) % 11;
 
-		int digito2 = 11 - valor2;
-		if ((soma2) % 11 < 2)
-			digito2 = 0;
-		return digito2;
+		d1 *= 2;
+		for (i = 0; i < 9; i++) {
+			d1 += Integer.parseInt(cpf1.substring(i, i + 1)) * (11 - i);
+		}
+
+		d1 = 11 - (d1 % 11);
+		if (d1 > 9) {
+			d1 = 0;
+		}
+
+		if (Integer.parseInt(cpf2.substring(1, 2)) != d1) {
+			return false;
+		}
+
+		return true;
 	}
 
 }
